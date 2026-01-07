adbfdc4b28b3a5d282a2e3cf1ad370434e57c533
kdesouza2 <keyra.desouza@gmail.com>
2026-01-07 18:06:53 -0500

    create in_class_exercises

---
+++ in_class_exercises/1-1.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+
+(define (isMember? x L)
+  (cond
+    [(null? L) #f]
+    [(equal? x (first L)) #t]
+    [else (isMember? x (rest L))]
+  )  
+)
+
+(define (removeDup L)
+  (if (null? L)
+      '()
+      (if (isMember? (first L) (rest L))
+          (removeDup (rest L))
+          (cons (first L) (removeDup (rest L)))))
+)
+
+(displayln (removeDup '(1 2 3 4 5 4 3 2 1)))
